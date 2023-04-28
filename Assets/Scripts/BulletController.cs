34b489cfe4456ff7fc734e6daaeb944f4a36d56d
madboy2022 <madgaze.artel@gmail.com>
2023-04-28 15:12:45 +0500

    added bullet lifetime and trigger enter in script

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,12 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private Rigidbody _rigidBody;
+    [SerializeField] private float _lifeTime;
+
+    private void Start()
+    {
+        StartCoroutine(DestroyCoroutine());
+    }
 
     private void Update()
     {
         _rigidBody.velocity = transform.forward * _moveSpeed;
+    }
+
+    private IEnumerator DestroyCoroutine()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
