a3db786ce62a2a6564db47720e608ed9696a1ef6
madboy2022 <madgaze.artel@gmail.com>
2023-04-28 17:02:07 +0500

    fixed bullet and particle with hometask destroyer script

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,27 +1,14 @@
-using System.Collections;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private Rigidbody _rigidBody;
-    [SerializeField] private float _lifeTime;
     [SerializeField] private GameObject _particleEffect;
-
-    private void Start()
-    {
-        StartCoroutine(DestroyCoroutine());
-    }
 
     private void Update()
     {
         _rigidBody.velocity = transform.forward * _moveSpeed;
-    }
-
-    private IEnumerator DestroyCoroutine()
-    {
-        yield return new WaitForSeconds(_lifeTime);
-        Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
