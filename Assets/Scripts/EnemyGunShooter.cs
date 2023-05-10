67df89d0dde5abceaf5489ba0c41f09a01d22f2b
max <109784025+madboy2022@users.noreply.github.com>
2023-05-10 16:48:30 +0500

    Update EnemyGunShooter.cs

--- Assets/Scripts/EnemyGunShooter.cs
+++ Assets/Scripts/EnemyGunShooter.cs
@@ -6,8 +6,28 @@ public class EnemyGunShooter : MonoBehaviour
     [SerializeField] private Transform _firePoint;
     [SerializeField] private EnemyMoveController _enemy;
 
+    private PlayerController _player;
+    private float _maxAngleToShot = 30f;
+
+    private void Start()
+    {
+        _player = PlayerController.instance;
+    }
+
     public void TryFire()
     {
-        Instantiate(_bullet, _firePoint.position, _firePoint.rotation);
+        _firePoint.LookAt(_player.transform.position + new Vector3(0f, 1.2f, 0f));
+
+        //Check the angle to the player
+        Vector3 targetDirecton = _player.transform.position - _enemy.transform.position;
+        float angleToPlayer = Vector3.SignedAngle(targetDirecton, _enemy.transform.forward, Vector3.up);
+        if(Mathf.Abs(angleToPlayer) < _maxAngleToShot)
+        {
+            Instantiate(_bullet, _firePoint.position, _firePoint.rotation);
+        }
+        else
+        {
+            _enemy.DontShot();
+        }
     }
 }
